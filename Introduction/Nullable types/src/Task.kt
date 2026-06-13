cf67c098581b9a8b06b8c3493eb4001474a6d833
Joseph Koch <joseph.koch55@gmail.com>
2026-06-13 11:21:45 -0400

    Initial commit

---
+++ Introduction/Nullable types/src/Task.kt
@@ -0,0 +1,14 @@
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) : Unit {
+    val email = client?.personalInfo?.email
+    if (message == null || email == null) return
+
+    mailer.sendMessage(email, message)
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
